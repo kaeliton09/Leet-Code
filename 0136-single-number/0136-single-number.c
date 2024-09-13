ad43b00d002b01e5531305a6af2fdf4b0a7d4762
Kaeliton <144848870+kaeliton09@users.noreply.github.com>
2024-09-13 19:52:48 -0300

    Time: 11 ms (84.68%), Space: 9.2 MB (59.92%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,12 +1,7 @@
 int singleNumber(int* nums, int numsSize) {
     int number = 0;
-    int vezes = 0;
     for(int i = 0; i < numsSize; i++){
-        vezes = 0;
-        for(int j = 0; j < numsSize; j++){
-            if(nums[i] == nums[j]) number = nums[i], vezes++;
-        }
-        if(vezes == 1) return number;
+        number ^= nums[i];
     }
     return number;
 }
